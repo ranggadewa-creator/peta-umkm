d31bcb70f53afe2aad231a0f74a56afe3280b08e
ranggadewa-creator <yasiratrangga154@gmail.com>
2026-07-29 23:29:08 +0700

    Set up map with layers and village boundaries
    Initialize map with OpenStreetMap and satellite layers, and load village boundary data from GeoJSON.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,58 @@
-console.log("WebGIS UMKM");
+// ===============================
+// MEMBUAT PETA
+// ===============================
+
+var map = L.map('map');
+
+
+// ===============================
+// BASEMAP
+// ===============================
+
+var osm = L.tileLayer(
+'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+{
+    attribution:'© OpenStreetMap'
+});
+
+var satellite = L.tileLayer(
+'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+{
+    attribution:'Esri'
+});
+
+osm.addTo(map);
+
+
+// ===============================
+// STYLE BATAS DESA
+// ===============================
+
+function styleBatas(feature){
+
+    return{
+        color:'green',
+        weight:3,
+        fill:false
+    };
+
+}
+
+
+// ===============================
+// LOAD BATAS DESA
+// ===============================
+
+var batasLayer;
+
+fetch("data/batas_desa.geojson")
+.then(response=>response.json())
+.then(function(data){
+
+    batasLayer=L.geoJSON(data,{
+        style:styleBatas
+    }).addTo(map);
+
+    map.fitBounds(batasLayer.getBounds());
+
+});
