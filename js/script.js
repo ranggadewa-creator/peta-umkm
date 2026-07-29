1c3f0045537716537f22f4cc288c3c967643587c
KELANAHARA <yasiratrangga154@gmail.com>
2026-07-29 23:57:15 +0700

    Implement geoJSON loading for village and UMKM layers
    Add functionality to load and display geoJSON data for village boundaries and UMKM on the map with custom styles.

--- js/script.js
+++ js/script.js
@@ -40,3 +40,78 @@ osm.addTo(map);
 // Variabel layer
 var batasLayer;
 var umkmLayer;
+
+// ======================================
+// STYLE BATAS DESA
+// ======================================
+
+function styleBatas() {
+    return {
+        color: "#00aa00",
+        weight: 3,
+        fillColor: "#00ff00",
+        fillOpacity: 0.10
+    };
+}
+
+// ======================================
+// MEMUAT DATA
+// ======================================
+
+Promise.all([
+    fetch("data/batas_desa.geojson").then(res => res.json()),
+    fetch("data/UMKM_Geojson.geojson").then(res => res.json())
+])
+
+.then(([batasData, umkmData]) => {
+
+    // ===============================
+    // BATAS DESA
+    // ===============================
+
+    batasLayer = L.geoJSON(batasData, {
+        style: styleBatas
+    });
+
+    batasLayer.addTo(map);
+
+    map.fitBounds(batasLayer.getBounds());
+
+    // ===============================
+    // UMKM
+    // ===============================
+
+    umkmLayer = L.geoJSON(umkmData, {
+
+        pointToLayer: function(feature, latlng){
+
+            return L.circleMarker(latlng,{
+                radius:7,
+                fillColor:"#ff0000",
+                color:"#ffffff",
+                weight:2,
+                fillOpacity:1
+            });
+
+        },
+
+        onEachFeature:function(feature,layer){
+
+            let p = feature.properties;
+
+            layer.bindPopup(`
+                <center>
+                    <img src="${p.Foto}" width="220"><br><br>
+                    <b>${p["Nama Usaha"]}</b><br><br>
+                    <b>Pemilik :</b> ${p.Pemilik}<br><br>
+                    ${p.Deskripsi}
+                </center>
+            `);
+
+        }
+
+    });
+
+    umkmLayer.addTo(map);
+
+});
